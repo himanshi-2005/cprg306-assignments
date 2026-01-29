8a14eb0f306f19b73e65c953cc94476f60212020
himanshi-2005 <himanshihimanshi631@gmail.com>
2026-01-29 11:40:06 -0700

    Update text color in grocery item

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -1,7 +1,7 @@
 export default function Item({ name, quantity, category }) {
   return (
     <li className="border p-3 rounded-md bg-slate-100">
-      <p className="font-semibold">{name}</p>
+      <p className="font-semibold text-black">{name}</p>
       <p className="text-sm text-gray-600">
         Quantity: {quantity} | Category: {category}
       </p>
