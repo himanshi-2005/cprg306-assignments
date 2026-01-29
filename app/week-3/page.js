4868268146aeef18601dc30c0d74ee1a0d4bb420
himanshi-2005 <himanshihimanshi631@gmail.com>
2026-01-29 11:26:46 -0700

    Week 3 link added in the main page

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,8 +3,8 @@ import GroceryItemList from "./GroceryItemList";
 export default function Page() {
   return (
     <main className="p-6 max-w-xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6 text-center">
-        Shopping List
+      <h1 className="text-3xl font-bold mb-6 text-center text-black">
+     Shopping List
       </h1>
 
       <GroceryItemList />
