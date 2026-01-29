547208a765de871b9f87a0838ed5ec8b4ec212e9
himanshi-2005 <himanshihimanshi631@gmail.com>
2026-01-29 11:43:31 -0700

    Update text colour of heading

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import GroceryItemList from "./GroceryItemList";
 export default function Page() {
   return (
     <main className="p-6 max-w-xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6 text-center text-black">
+      <h1 className="text-3xl font-bold mb-6 text-center text-white">
      Shopping List
       </h1>
 
