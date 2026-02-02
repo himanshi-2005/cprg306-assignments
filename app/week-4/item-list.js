3bd31425285399b2fd8612592e0802f65f066e36
himanshi-2005 <himanshihimanshi631@gmail.com>
2026-02-02 11:12:22 -0700

    updated the 4 created file

---
+++ app/week-4/item-list.js
@@ -0,0 +1,17 @@
+import Item from "./item";
+import items from "./items.json";
+
+export default function ItemList() {
+  return (
+    <ul>
+      {items.map((item) => (
+        <Item
+          key={item.id}
+          name={item.name}
+          quantity={item.quantity}
+          category={item.category}
+        />
+      ))}
+    </ul>
+  );
+}
