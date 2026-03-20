14c91c9301148f4374d03ade94ac3fc20509be48
himanshi-2005 <himanshihimanshi631@gmail.com>
2026-03-20 03:20:29 -0600

    page file code is updated

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,27 +1,27 @@
 "use client";
 
+import Link from "next/link";
 import { useUserAuth } from "../contexts/AuthContext";
 
-export default function Page() {
+export default function LandingPage() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
-
-  const handleLogin = async () => {
-    await gitHubSignIn();
-  };
-
-  const handleLogout = async () => {
-    await firebaseSignOut();
-  };
 
   return (
     <div>
-      <button onClick={handleLogin}>Login with GitHub</button>
-      <button onClick={handleLogout}>Logout</button>
+      {!user ? (
+        <button onClick={gitHubSignIn}>Login with GitHub</button>
+      ) : (
+        <div>
+          <p>
+            Welcome, {user.displayName} 
+          </p>
 
-      {user && (
-        <p>
-          Welcome, {user.displayName} ({user.email})
-        </p>
+          <button onClick={firebaseSignOut}>Logout</button>
+
+          <br /><br />
+
+          <Link href="/week-9/shopping-list">Go to Shopping List</Link>
+        </div>
       )}
     </div>
   );
